dacb1608e8df42d81dcb451756ca94806518dfa3
Ludovic <dovmailbox@gmail.com>
2019-04-04 10:09:39 +0200

    Update photo_tpl.php

--- templates/photo_tpl.php
+++ templates/photo_tpl.php
@@ -9,7 +9,6 @@
 
 	<nav class="toolbar">
 		<button id="slider-previous"><i class="fas fa-backward"></i></button>
-		<button id="slider-toggle"><i class="fas fa-play"></i></button>
 		<button id="slider-next"><i class="fas fa-forward"></i></button>
 	</nav>
 
